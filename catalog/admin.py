fea6a64b88ed8d17aaa9af7385262e214a88232e
Nikolai <kolamba.r@gmail.com>
2023-08-17 22:07:10 +0300

    hw 5 before slug in address

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Blog
 
 
 # Register your models here.
@@ -16,3 +16,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'cost', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'header', 'slug', 'creation_date',)
+    list_filter = ('header', 'creation_date')
+    search_fields = ('name', 'text',)
